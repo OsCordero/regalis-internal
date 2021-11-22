77d16ab4f50ed2639692e08bcc1c3078c7717241
GabrielSR07 <GabrielSR07@users.noreply.github.com>
2021-11-21 23:11:15 -0400

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,30 @@
 import type { AppProps } from "next/app";
 import { MoralisProvider } from "react-moralis";
-import "../src/styles/global.css";
+import { useEffect } from "react";
 import "tailwindcss/tailwind.css";
 import "../src/styles/additional-styles/range-slider.scss";
 import "../src/styles/additional-styles/theme.scss";
 import "../src/styles/additional-styles/toggle-switch.scss";
 import "../src/styles/additional-styles/utility-patterns.scss";
+import AOS from "aos";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    AOS.init({
+      once: true,
+      disable: "phone",
+      duration: 700,
+      easing: "ease-out-cubic",
+    });
+  }, []);
+
+  // useEffect(() => {
+  //   document.querySelector("html").style.scrollBehavior = "auto";
+  //   window.scroll({ top: 0 });
+  //   document.querySelector("html").style.scrollBehavior = "";
+  //   focusHandling("outline");
+  // }, [location.pathname]);
+
   return (
     <MoralisProvider
       appId={process.env.NEXT_PUBLIC_APP_ID!}
