81e07e9e4a3c07db0e69724518e4c6c8af189ec4
GabrielSR07 <GabrielSR07@users.noreply.github.com>
2021-12-22 12:00:13 -0400

    update

--- src/eth/forwarder.ts
+++ src/eth/forwarder.ts
@@ -1,6 +1,6 @@
 import { ethers } from "ethers";
 import { forwardedAbi } from "../constants/abi-forwarded";
-const address = "0xf8F38887ea903eBF82bc9069dFB782cc517baDeC";
+const address = "0xEf6ac4ce9eF1A18128624EB090FA74B7176132c7";
 
 export function createInstance(provider: any) {
   return new ethers.Contract(address, forwardedAbi, provider);
